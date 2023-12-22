833968530ab492568763cbd4c841d07f7b4dfd34
Alish1660 <alirustamov1660@gmail.com>
2023-12-22 06:01:12 +0300

    backtop

--- main.js
+++ main.js
@@ -12,3 +12,22 @@ remove.addEventListener("click", () => {
   // nav_items.style = "transform: translateY(0%)";
   console.log(1);
 });
+
+
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.style.bottom = "20px";
+  } else {
+    backtop.style.bottom = "-50px";
+  }
+}
+backtop.addEventListener("click", () => window.scrollTo(0, 0));
